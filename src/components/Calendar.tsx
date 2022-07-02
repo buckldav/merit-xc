2fc53a409aba8fff2fa71ea8be6f2871cb5b5511
David Buckley <runalldayandnight@gmail.com>
2022-07-02 13:50:19 -0600

    Taller calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,7 +3,7 @@ const Map = () => (
     title="workout calendar"
     src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FDenver&amp;src=Y185aHU4c2M0YzFubGM3N25va200a3ZpdTZpb0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%239E69AF&amp;showNav=0&amp;showTitle=0&amp;showDate=1&amp;showPrint=0&amp;showCalendars=1&amp;mode=AGENDA"
     width={330}
-    height={230}
+    height={300}
     className="w-full"
   />
 );
