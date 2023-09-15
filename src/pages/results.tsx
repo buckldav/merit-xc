8836b5577bb5f2773fe01f85a27dad7689db50fb
David Buckley <runalldayandnight@gmail.com>
2023-09-14 21:40:54 -0600

    jr high results

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -218,6 +218,7 @@ const Results = (props: ResultsProps) => {
         >
           <optgroup label="Race Results">
             <option value="2023 5ks">2023 Results</option>
+            <option value="2023 Junior High">2023 Jr. High Results</option>
             <option value="2022 5ks">2022 Results</option>
             <option value="2022 Junior High">2022 Jr. High Results</option>
             <option value="2021 5ks">2021 Results</option>
